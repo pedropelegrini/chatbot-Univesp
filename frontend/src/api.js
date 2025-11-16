20a33c786b0bdf3364e2d453306f6776ab70f6dc
Pedro <contatopelegrini@gmail.com>
2025-11-15 22:08:20 -0300

    URL Render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000"; // ou http://localhost:8000
+const API_URL = "https://chatbot-univesp-x3.onrender.com";
 
 export const sendMessageToGemini = async (message) => {
   try {
